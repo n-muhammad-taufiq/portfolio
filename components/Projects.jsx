112d42a3dfb9f935af642c750c993288ea19fd92
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-09-14 19:28:19 +0530

    fix: resolve broken image paths in production build

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -10,22 +10,22 @@ const Projects = () => {
         <p className='text-2xl max-lg:text-xl font-bold '>Cerfira - a Social Media Platform</p>
         <div className='rounded-md w-full grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5'>
             <img className='rounded-md  border-2 border-cyan-600 object-cover  h-full w-full col-span-full' src="./src/assets/cerfira/Screenshot-1.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full ' src="./src/assets/cerfira/Screenshot-2.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-3.png" alt="" />
-            <img className='rounded-md border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-4.png" alt="" />
-            <img className='rounded-md border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-6.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-7.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-8.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-9.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-10.png" alt="" />
-            <img className='rounded-md border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-11.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-12.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-13.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-14.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-15.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-16.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="./src/assets/cerfira/Screenshot-17.png" alt="" />
-            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full ' src="./src/assets/cerfira/Screenshot-18.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full ' src="/project-images/cerfira/Screenshot-2.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-3.png" alt="" />
+            <img className='rounded-md border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-4.png" alt="" />
+            <img className='rounded-md border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-6.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-7.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-8.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-9.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-10.png" alt="" />
+            <img className='rounded-md border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-11.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-12.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-13.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-14.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-15.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-16.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full' src="/project-images/cerfira/Screenshot-17.png" alt="" />
+            <img className='rounded-md  border-2 border-cyan-600 object-cover h-full w-full ' src="/project-images/cerfira/Screenshot-18.png" alt="" />
         </div>
       </div>
 
